c7ea6cc406b667bc043c60c01338932cfa615bf8
Sam Wells <32932600+sllewmas@users.noreply.github.com>
2025-01-14 12:33:04 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
+
+let song = {
+    title: "Apple",
+    album: "Brat",
+    releaseYear: 2023,
+    isAvailable: false
+}
+
+//↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
+
+
